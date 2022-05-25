af6353060d4ee74ce567d485c4efcef942ce5039
RamonSandoval <102700747+RamonSandoval@users.noreply.github.com>
2022-05-24 20:15:36 -0700

    removed

--- lib/pages/tabla.dart
+++ lib/pages/tabla.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:proyecto_futbol/services/football_service.dart';
 
 class Tabla extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final footballDataService = Provider.of<FootBallServices>(context);
+    //print(footballDataService.propiedadesJugador[0].country);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Tabla General Liga MX ',
         ),
         backgroundColor: Color(0xFF10203c),
+      ),
+      body: Container(
+        child: Text(''),
       ),
     );
   }
--- lib/services/football_service.dart
+++ lib/services/football_service.dart
@@ -2,29 +2,14 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:proyecto_futbol/models/ptrophy_modal.dart';
 
-import '../models/tabla_modal.dart';
-
 class FootBallServices extends ChangeNotifier {
   final String _urlBase = 'api-football-v1.p.rapidapi.com';
   final String _apiKey = '069c410e33msh94b5d9703c72a69p1cd47djsn5f6506384e39';
 
   List<Response> propiedadesJugador = [];
-  List<List<Standing>> propiedadesTabla = [];
 
   FootBallServices() {
     getServicePlayer();
-    getServiceTabla();
-  }
-
-  //Funcion que se conecta a la api lista tabla
-  getServiceTabla() async {
-    final url = Uri.https(
-        _urlBase, '/v3/standings', {'season': '2021', 'league': '262'});
-    final respuesta = await http.get(url, headers: {'X-RapidApi-Key': _apiKey});
-
-    final tabla = TablaGeneral.fromJson(respuesta.body);
-    propiedadesTabla = tabla.standings!;
-    print(propiedadesTabla);
   }
 
 //Funcion que se conecta a la api lista de player
